7c899e8938cddf17edad41a5d196d162b1cc25ee
DESKTOP-U2D04CL\Szymon <szymon.brzozowski14@gmail.com>
2021-03-20 10:58:30 +0100

    refactor views

--- Aplikacja/backend/BackendAPI/RoadSignsApp/urls.py
+++ Aplikacja/backend/BackendAPI/RoadSignsApp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^roadSign/$',views.roadSignsApi),
-    url(r'^roadSign/([0-9]+)$',views.roadSignsApi),
+    url(r'^roadSign/$',views.roadSignList.as_view()),
+    url(r'^roadSign/(?P<pk>[0-9]+)$',views.roadSignAction.as_view()),
     url(r'^SaveFile$',views.SaveFile)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Aplikacja/backend/BackendAPI/RoadSignsApp/views.py
+++ Aplikacja/backend/BackendAPI/RoadSignsApp/views.py
@@ -2,41 +2,22 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
-
+from rest_framework import generics
 from RoadSignsApp.models import RoadSigns
 from RoadSignsApp.serializers import RoadSignSerializer
 
 from django.core.files.storage import default_storage
 
 # Create your views here.
-@csrf_exempt
-def roadSignsApi(request,id=0):
-    if request.method =='GET':
-       roadsigns = RoadSigns.objects.all()
-       roadsigns_serializer = RoadSignSerializer(roadsigns,many = True)
-       return JsonResponse(roadsigns_serializer.data, safe=False)
-    
-    elif request.method=='POST':
-        roadsign_data = JSONParser().parse(request)
-        roadsign_serializer = RoadSignSerializer(data=roadsign_data)
-        if roadsign_serializer.is_valid():
-            roadsign_serializer.save()
-            return JsonResponse("added Successfully!!!",safe=False)
-        return JsonResponse("Failed to add!",safe=False)
-    
-    elif request.method=='PUT':
-        roadsign_data = JSONParser().parse(request)
-        roadsign=RoadSigns.objects.get(RoadSignId=roadsign_data['RoadSignId'])    
-        roadsign_serializer = RoadSignSerializer(roadsign,data=roadsign_data)
-        if roadsign_serializer.is_valid():
-            roadsign_serializer.save()
-            return JsonResponse("Updated Successfully!!!",safe=False)
-        return JsonResponse("Failed to Update!",safe=False)
 
-    elif request.method =="DELETE":
-        roadsign = RoadSigns.objects.get(RoadSignId=id)
-        roadsign.delete()
-        return JsonResponse("Deleted SuccessFully!!!",safe=False)
+class roadSignList(generics.ListAPIView):
+      queryset=RoadSigns.objects.all()
+      serializer_class = RoadSignSerializer
+
+class roadSignAction(generics.RetrieveUpdateDestroyAPIView):
+      queryset=RoadSigns.objects.all()
+      serializer_class = RoadSignSerializer 
+      lookup_fields = 'RoadSignId'     
 
 @csrf_exempt
 def SaveFile(request):
