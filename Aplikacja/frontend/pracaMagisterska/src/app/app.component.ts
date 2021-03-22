79fcd9ea209a356803fab4ed0a4df64481eb5102
DESKTOP-U2D04CL\Szymon <szymon.brzozowski14@gmail.com>
2021-03-22 20:20:12 +0100

    hide navbar when user is not logged in

--- Aplikacja/frontend/pracaMagisterska/src/app/app.component.ts
+++ Aplikacja/frontend/pracaMagisterska/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './core/models/user.model';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pracaMagisterska';
+  currentUser: User;
+  constructor(
+    private authenticationService: AuthService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 }
